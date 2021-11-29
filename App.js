487b838cb8d6c4e13a6c0b347f49e283a6213987
chinmay <chinmaymhatre12@gmail.com>
2021-11-29 12:21:59 +0530

    counter

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [counter,setCounter] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text 
+        style={styles.text}
+      >
+        {counter}
+      </Text>
+      <TouchableOpacity title="Increment" onPress={()=>setCounter(counter+1)}>
+        <Text style={styles.button}>Increment</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        title="Reset"
+        onPress={()=>setCounter(0)}
+      >
+        <Text style={styles.button}>Reset</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +33,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button:{
+    marginTop:20,
+    padding:10,
+    backgroundColor:'#ddd',
+    borderRadius:10,
+    width:100,
+    textAlign:'center'
+  },
+  text:{
+    fontSize:30,
+    paddingBottom:20
+  }
 });
